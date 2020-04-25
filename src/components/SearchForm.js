32624246a348393174fff6de2c6fc2f8e9522bd9
Scotty <Scott.p.mikul@gmail.com>
2020-04-25 17:10:09 -0400

    added a jumbotron to the search form. modified the readme to describe the project and link the live demo. Added an image of the working project

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,11 +1,15 @@
 import React from "react";
-
+import "./form.css";
 class SearchForm extends React.Component{
 
 
     render(){
         return(
             <div>
+                <div className="jumbotron">
+                    <h1 className="text-center">Employee Directory</h1>
+                    <h4>Type to search for an employee or phone number</h4>
+                </div>
                 <label htmlFor="search">Search</label>
                 <input className="ml-3" name="search" type="text" onChange={(event)=>{this.props.updateSearch(event.target.value)}}></input>
             </div>
